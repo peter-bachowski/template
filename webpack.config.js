1d62e685e8abe1b543b032232b6f902d2a3780be
Peter <bachowskip@gmail.com>
2023-08-28 23:12:55 +0700

    add babel packages

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,18 @@ module.exports = {
         test: /\.xml$/i,
         use: ['xml-loader'],
       },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ],
+          }
+        }
+      },
     ],
   },
 
